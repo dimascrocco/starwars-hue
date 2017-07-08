507421fe0313a4a13619189977baa9a7ea51c568
dimascrocco <dimas.crocco@gmail.com>
2017-07-08 15:40:17 -0300

    percorre video e exibe dados estatisticos sobre o conteudo

--- cinema.py
+++ cinema.py
@@ -2,7 +2,6 @@ import time
 import sys
 
 filme = open("starwars.txt")
-
 frames = filme.readlines()
 
 texto = ""
@@ -10,16 +9,12 @@ for f in frames:
     texto += f
 
 frames = texto.split("[H")
+
 print "frames", len(frames)
 
 
-
-frame = int(sys.argv[1])
-print "frame", frame
-print frames[frame]
-
-exit()
-
+def show(frame):
+    print frames[frame]
 
 def clear():
     print(chr(27) + "[2J")
@@ -30,10 +25,47 @@ def delay():
     duration = float(cfg[0])
     time.sleep(duration)
 
-for f in frames:
-    clear()
-    print f
-    try:
-        delay()
-    except:
-        print "" 
+def play():
+    for f in frames:
+        clear()
+        print f
+        try:
+            delay()
+        except:
+            print "" 
+
+def main():
+    symbols = set()
+    tf = {}
+    for f in frames:
+        for l in f:
+            for c in l:
+                symbols.add(c)
+                if not tf.get(c):
+                    tf[c] = 0
+                tf[c] += 1
+    del tf['\n']
+    print "n simbolos:", len(symbols)
+    print "simbolos:", "".join(sorted(symbols)).replace('\n', '')
+    tf = sorted(tf.items(), key=lambda x: x[1], reverse=True)
+
+    count = 0
+    print "frequencia dos simbolos:"
+    for k, v in tf:
+        print count, ord(k), "->", k, v
+        count += 1
+
+
+    last = 0
+    while True:
+        n = raw_input("({0}) frame: ".format(last))
+        try:
+            value = int(n)
+            show(value)
+            last = value
+        except:
+            last += 1
+            show(last)
+
+if __name__ == "__main__":
+    main()
